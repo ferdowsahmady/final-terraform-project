4fcce206dc98cb21226b730ab080378ddfcef411
ferdowsahmady <ferdowsahmady@gmail.com>
2024-08-21 22:06:01 -0400

    second commit

---
+++ environments/sbx/providers.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+backend "s3" {
+  bucket  = "terraform-state-file-code-killers"
+  key     = "terraform.tfstate"
+  region  = "us-east-1"
+  encrypt = true
+  # dynamodb_table = "terraform-state-lock"
+}
+}
