42121f397bb87295cd89445e46ec9dc36cfbb2b7
rpaskalev <rpaskalev@lomixtech.com>
2024-08-29 14:04:42 -0400

    testng

--- app1.sh
+++ app1.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-echo "Starting app1"
-sudo yum update -y
-sudo yum install -y httpd
-sudo yum install -y stress
-sudo yum install -y htop
-sudo mkdir /var/www/html/app1
-aws s3 cp s3://ferro-app1/index.html /var/www/html/app1/index.html
-sudo systemctl start httpd
-sudo systemctl enable httpd
-
 yum -y update
 yum install -y ruby
 yum install -y aws-cli
