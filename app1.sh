a7415e174e8aca0d5b948dcb3464a85979052af9
rpaskalev <rpaskalev@lomixtech.com>
2024-08-29 14:54:27 -0400

    testng

--- app1.sh
+++ app1.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-sudo yum update -y
-sudo yum install -y httpd
-sudo systemctl start httpd.service
-# sudo systemctl enable httpd.service
-sudo echo "<h1> At $(hostname -f) </h1>" > /var/www/html/index.html
-
 yum -y update
 yum install -y ruby
 yum install -y aws-cli
