3bd5d37ef60eed45901acf4ac9852ea2436f073e
ferdowsahmady <ferdowsahmady@gmail.com>
2024-08-30 00:50:07 -0400

    updating the appspect.yml

--- app1.sh
+++ app1.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
-echo "Starting app1"
-sudo yum update -y
-sudo yum install -y httpd
-sudo yum install -y stress
-sudo yum install -y htop
 sudo mkdir /var/www/html/app1
-aws s3 cp s3://ferro-app1/index.html /var/www/html/app1/index.html
-sudo systemctl start httpd
-sudo systemctl enable httpd
-sudo yum install -y aws-cli
-sudo yum install -y ruby
+yum -y update
+yum install -y ruby
+yum install -y aws-cli
 cd /home/ec2-user
 aws s3 cp s3://aws-codedeploy-us-east-1/latest/install . --region us-east-1
 chmod +x ./install
